5328bfc63f4a80e5060140788923272fbd23929b
Richard Todd <rwtodd@users.noreply.github.com>
2026-03-07 20:54:12 -0600

    added cmdline options to the driver program

--- cmd/go-forth/driver.go
+++ cmd/go-forth/driver.go
@@ -3,8 +3,10 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
+	"strings"
 
 	_ "github.com/rwtodd/Go.Forth/extensions/random"
 	_ "github.com/rwtodd/Go.Forth/extensions/regex"
@@ -13,24 +15,85 @@ import (
 )
 
 func main() {
+	var extensions []string
+	var preExprs []string
+	var postExprs []string
+	var interactive bool
+
+	flag.Func("x", "Comma-separated list of extensions to activate", func(s string) error {
+		exts := strings.Split(s, ",")
+		for _, ext := range exts {
+			ext = strings.TrimSpace(ext)
+			if ext != "" {
+				extensions = append(extensions, ext)
+			}
+		}
+		return nil
+	})
+
+	flag.Func("e", "Evaluate expression before loading files", func(s string) error {
+		preExprs = append(preExprs, s)
+		return nil
+	})
+
+	flag.Func("ee", "Evaluate expression after loading files", func(s string) error {
+		postExprs = append(postExprs, s)
+		return nil
+	})
+
+	flag.BoolVar(&interactive, "i", false, "Run interactively after processing args")
+	flag.Parse()
+
+	files := flag.Args()
+
 	vm := forth.NewVM(os.Stdin, os.Stdout)
 
-	// Parse arguments as files to load before interactive prompt
-	if len(os.Args) > 1 {
-		for _, arg := range os.Args[1:] {
-			if err := vm.Load(arg); err != nil {
-				fmt.Fprintf(os.Stderr, "Error loading %s: %v\n", arg, err)
-				os.Exit(1)
-			}
+	// 1. Activate extensions
+	if len(extensions) > 0 {
+		for _, ext := range extensions {
+			vm.Push(ext)
+		}
+		vm.Push(int64(len(extensions)))
+		if err := vm.Run(strings.NewReader("<activate-extensions>"), "cmdline"); err != nil {
+			fmt.Fprintf(os.Stderr, "Error activating extensions: %v\n", err)
+			os.Exit(1)
 		}
 	}
 
-	for {
-		err := vm.Run(os.Stdin, "stdin")
-		if err == nil {
-			break
+	// 2. Pre-expressions
+	for _, expr := range preExprs {
+		if err := vm.Run(strings.NewReader(expr), "cmdline"); err != nil {
+			fmt.Fprintf(os.Stderr, "Error evaluating -e '%s': %v\n", expr, err)
+			os.Exit(1)
 		}
-		fmt.Fprintf(os.Stderr, "Error: %v\n\n", err)
-		vm.ResetState()
+	}
+
+	// 3. Load files
+	for _, file := range files {
+		if err := vm.Load(file); err != nil {
+			fmt.Fprintf(os.Stderr, "Error loading %s: %v\n", file, err)
+			os.Exit(1)
+		}
+	}
+
+	// 4. Post-expressions
+	for _, expr := range postExprs {
+		if err := vm.Run(strings.NewReader(expr), "cmdline"); err != nil {
+			fmt.Fprintf(os.Stderr, "Error evaluating -ee '%s': %v\n", expr, err)
+			os.Exit(1)
+		}
+	}
+
+	// 5. Interactive prompt
+	hasArgs := len(files) > 0 || len(preExprs) > 0 || len(postExprs) > 0 || len(extensions) > 0
+	if interactive || !hasArgs {
+		for {
+			err := vm.Run(os.Stdin, "stdin")
+			if err == nil {
+				break
+			}
+			fmt.Fprintf(os.Stderr, "Error: %v\n\n", err)
+			vm.ResetState()
+		}
 	}
 }
